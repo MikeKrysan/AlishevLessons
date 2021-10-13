49535916b8063a752842d49b77af9b10766f457d
MikeKrysan <krysanmihasan@gmail.com>
2021-10-13 15:22:10 +0300

    AlishevLessons firstcommit

--- src/Dog.java
+++ src/Dog.java
@@ -5,6 +5,9 @@ public class Dog extends Animal{
     public void bark(){
         System.out.println("I am barking");
     }
+    public void pee() {
+        System.out.println("I'm pee-pee");
+    }
 
     public void showName(){
         System.out.println();
